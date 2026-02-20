656ac6ca233f6e49494b1f8f8e1364f12cd17892
felix43425021-RE-Polines <felix.43425021@mhs.polines.ac.id>
2026-02-20 22:31:56 +0700

    rollback

--- lib/view.dart
+++ lib/view.dart
@@ -1,13 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
-abstract class StatelessView extends StatelessWidget {
-  const StatelessView({super.key, this.firestoreData, this.realtimeData});
-  final dynamic firestoreData;
-  final dynamic realtimeData;
+abstract class View extends StatelessWidget {
+  const View({super.key, this.firestoreData, this.realtimeData});
+  final Map<String, Future<QuerySnapshot>>? firestoreData;
+  final Map<String, Stream<DatabaseEvent>>? realtimeData;
 }
 
 abstract class StatefulView extends StatefulWidget {
   const StatefulView({super.key, this.firestoreData, this.realtimeData});
-  final dynamic firestoreData;
-  final dynamic realtimeData;
+  final Map<String, Future<QuerySnapshot>>? firestoreData;
+  final Map<String, Stream<DatabaseEvent>>? realtimeData;
 }
